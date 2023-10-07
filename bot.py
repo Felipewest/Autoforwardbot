9eb0e40f119c5d003c5e901180a46d16184a9ad6
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-10-07 20:42:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import os
 import psutil
 import telegram
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram import ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext, CallbackQueryHandler
 from pymongo import MongoClient
 from config import TELEGRAM_BOT_TOKEN, DUMMY_BOT_TOKEN, MONGODB_URI, API_ID, API_HASH, PYROGRAM_STRING_SESSION
